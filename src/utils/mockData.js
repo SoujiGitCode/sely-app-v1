93f961ff34ec5be8bcb9eb9824a0155ad78e7dd9
freelf <liseth.yggdrasil@gmail.com>
2023-07-26 20:49:36 -0400

    feat: new charts proposal

---
+++ src/utils/mockData.js
@@ -0,0 +1,34 @@
+export const staticBlankLimit = [
+    { x: 'A1', y: 110 },
+    { x: 'B1', y: 110 },
+    { x: 'C1', y: 110 },
+    { x: 'D1', y: 110 },
+]
+
+export const defaultMultiBar = [
+    [
+        { x: 'A1', y: 100 },
+        { x: 'B1', y: 100 },
+        { x: 'C1', y: 100 },
+        { x: 'D1', y: 100 },
+    ],
+    [
+        { x: 'A1', y: 20 },
+        { x: 'B1', y: 20 },
+        { x: 'C1', y: 20 },
+        { x: 'D1', y: 20 },
+    ],
+    [
+        { x: 'A1', y: 50 },
+        { x: 'B1', y: 50 },
+        { x: 'C1', y: 50 },
+        { x: 'D1', y: 50 },
+    ],
+]
+
+export const defaultData = [
+    { x: 'A1', y: 90 },
+    { x: 'B1', y: 90 },
+    { x: 'C1', y: 90 },
+    { x: 'D1', y: 90 },
+];
